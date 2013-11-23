2707eb199a2d009bce66faf27b35c673ea75e074
Philip Neustrom <philipn@gmail.com>
2013-11-23 02:14:36 -0800

    testing travis fix

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -267,7 +267,7 @@ def install_system_requirements():
     ]
     solr_pkg = ['solr-jetty', 'default-jre-headless']
     apache_pkg = ['apache2', 'libapache2-mod-wsgi']
-    postgres_pkg = ['libgdal1h', 'gdal-bin', 'proj', 'postgresql-9.1-postgis-2.0']
+    postgres_pkg = ['gdal-bin', 'proj', 'postgis']
     memcached_pkg = ['memcached']
     varnish_pkg = ['varnish']
     redis_pkg = ['redis-server']
